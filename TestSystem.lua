32ecd5e705c1271703b2fe3d019a2bac32c82576
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-10-31 18:49:52 -0300

    Update TestSystem.lua

--- TestSystem.lua
+++ TestSystem.lua
@@ -3,8 +3,17 @@ local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shle
 
 -- Puxa o script de elementos da API no GitHub e chama a função para adicionar os elementos
 local AdicionarElementos = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/MsWorking/refs/heads/main/teste/MsText.lua"))()
-local Window = OrionLib:MakeWindow({Name = "Minha Janela com API", HidePremium = false, SaveConfig = true, ConfigFolder = "Config"})
 
+-- Cria uma janela usando o Orion Lib
+local Window = OrionLib:MakeWindow({
+    Name = "Minha Janela com API",
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = "Config"
+})
+
+-- Chama a função para adicionar os elementos à janela
 AdicionarElementos(Window)
 
+-- Inicializa o Orion Lib
 OrionLib:Init()
