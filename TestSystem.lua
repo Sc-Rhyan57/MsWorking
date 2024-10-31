3b42d61270439a70b8d6e4404cfc9d18cd51b156
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-10-31 17:54:01 -0300

    Update TestSystem.lua

--- TestSystem.lua
+++ TestSystem.lua
@@ -2,15 +2,13 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 
 -- Carregando o sistema do GitHub
-loadstring(game:HttpGet("link_do_repositorio_github"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/MsWorking/refs/heads/main/teste/"))()
 
--- Definindo variáveis para os elementos
+
 local Elementos = {
-    ["Elemento1"] = "https://raw.githubusercontent.com/usuario/repositorio/main/arquivo1.lua",
-    ["Elemento2"] = "https://raw.githubusercontent.com/usuario/repositorio/main/arquivo2.lua"
+    ["Elemento1"] = "https://raw.githubusercontent.com/Sc-Rhyan57/MsWorking/refs/heads/main/teste/MsText.lua"
 }
 
--- Função para carregar os elementos
 function carregarElemento(elemento)
     local sucesso, conteudo = pcall(function()
         return loadstring(game:HttpGet(Elementos[elemento]))()
@@ -23,11 +21,5 @@ function carregarElemento(elemento)
     end
 end
 
--- Criando uma janela (exemplo de uso da Orion Lib)
 local Window = OrionLib:MakeWindow({Name = "Minha Janela", HidePremium = false, SaveConfig = true})
-
--- Chamando elementos após a criação da janela
 carregarElemento("Elemento1")
-carregarElemento("Elemento2")
-
--- Código adicional da janela aqu
